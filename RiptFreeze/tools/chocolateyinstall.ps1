a0508cb2aaff0134302a97ec9db7b4c8d48f5e9b
tungsfer512 <tungbv5122001@gmail.com>
2024-02-24 23:21:56 +0700

    first commit

---
+++ RiptFreeze/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/tungsfer512/ript_install/raw/main/Setup_TimeFreeze.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'RiptFreeze*'
+
+  silentArgs   = '/VERYSILENT' # Inno Setup
+}
+
+Install-ChocolateyPackage @packageArgs
